acdbe1c32a1547d5a3880da13bee90dfe6c346e7
Aanand <aanandab44@gmail.com>
2026-08-24 16:26:13 +0530

    Upgrade scaffold to Flutter 3.47.1 (AGP 9.1.0, Kotlin 2.4.0, Gradle 9.3.1, Kotlin DSL)

--- apps/customer_app/android/app/src/main/kotlin/com/onamflowers/customer_app/MainActivity.kt
+++ apps/customer_app/android/app/src/main/kotlin/com/onamflowers/customer_app/MainActivity.kt
@@ -2,4 +2,4 @@ package com.onamflowers.customer_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
--- apps/delivery_app/android/app/src/main/kotlin/com/onamflowers/delivery_app/MainActivity.kt
+++ apps/delivery_app/android/app/src/main/kotlin/com/onamflowers/delivery_app/MainActivity.kt
@@ -2,4 +2,4 @@ package com.onamflowers.delivery_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
